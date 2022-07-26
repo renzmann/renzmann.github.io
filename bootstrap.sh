d24cde1a67f4a2e9c3ec2fe2721f18e1f38e8ebc
renzmann <renzmann@users.noreply.github.com>
2022-07-26 01:07:00 +0000

    deploy: 556a57e1029023b88b5ca70f573505edbdce4a12

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 [ -d .vim ] && rm -r .vim
 [ -d .config/nvim ] && rm -r .config/nvim
+[ -d .emacs.d ] && rm -r .emacs.d
 git init
 git remote add origin git@github.com:renzmann/renzmann
 git pull
