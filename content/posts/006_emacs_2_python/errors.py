e6254f05c224b2e76834916a934788c3d6f971d1
Robb Enzmann <robbenzmann@gmail.com>
2022-08-10 12:07:57 -0400

    discovered a few more things

---
+++ content/posts/006_emacs_2_python/errors.py
@@ -0,0 +1,17 @@
+# [[file:index.org::*Some should only be errors if the =poetry= environment isn't activated][Some should only be errors if the =poetry= environment isn't activated:1]]
+import typing
+
+import requests
+import aaaaaaa
+
+foo
+print(typing.fdafdsafdsafdsafdsafdsafdsa)
+thing = "what"
+
+def myguy(foo: int = 0):
+    """My guy is always there for me."""
+
+
+def whatever(x: str) -> str:
+    return x + 1
+# Some should only be errors if the =poetry= environment isn't activated:1 ends here
