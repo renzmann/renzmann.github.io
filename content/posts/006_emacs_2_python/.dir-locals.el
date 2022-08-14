3197df2810535d12ce9c3b358d74a72b002ef858
Robb Enzmann <robbenzmann@gmail.com>
2022-08-13 21:10:41 -0400

    update dir local with mypy command

--- content/posts/006_emacs_2_python/.dir-locals.el
+++ content/posts/006_emacs_2_python/.dir-locals.el
@@ -3,4 +3,5 @@
 ;; 2. tried setting exec-path, but that's only for specific commands, like =M-x grep=
 ;; 3. had to read up on the alist and .dir-locals.el examples/syntax
 ;; 4. exec-path and PATH aren't necessarily in sync
-((nil . ((eval . (setenv "PATH" (concat "/home/robb/repos/renzmann.github.io/content/posts/006_emacs_2_python/.venv/bin" ":" (getenv "PATH")))))))
+((nil . ((eval . (setenv "PATH" (concat "/home/robb/repos/renzmann.github.io/content/posts/006_emacs_2_python/.venv/bin" ":" (getenv "PATH"))))
+	 (python-check-command . "mypy"))))
