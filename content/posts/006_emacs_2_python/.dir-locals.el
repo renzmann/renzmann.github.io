544ff263f5382a66a4b1b8401e314669cc192b5b
Robb Enzmann <robbenzmann@gmail.com>
2022-08-14 18:08:23 -0400

    update project files

--- content/posts/006_emacs_2_python/.dir-locals.el
+++ content/posts/006_emacs_2_python/.dir-locals.el
@@ -3,5 +3,9 @@
 ;; 2. tried setting exec-path, but that's only for specific commands, like =M-x grep=
 ;; 3. had to read up on the alist and .dir-locals.el examples/syntax
 ;; 4. exec-path and PATH aren't necessarily in sync
-((nil . ((eval . (setenv "PATH" (concat "/home/robb/repos/renzmann.github.io/content/posts/006_emacs_2_python/.venv/bin" ":" (getenv "PATH"))))
-	 (python-check-command . "mypy"))))
+;; 5. "mypy" and "pyright" aren't one of the default python-check-commmand programs
+
+;; This STILL doesn't work, because it won't be buffer-local. i.e. switching back to init.el will still have this venv at the front of PATH
+;; ((nil . ((eval . (setenv "PATH" (concat "/home/robb/repos/renzmann.github.io/content/posts/006_emacs_2_python/.venv/bin" ":" (getenv "PATH"))))
+;; 	 (python-check-command . "mypy"))))
+((python-mode . ((python-check-command . "mypy"))))
