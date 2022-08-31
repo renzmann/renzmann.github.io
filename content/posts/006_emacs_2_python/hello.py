6921572d5bfb54b59278d863c3a2ad7a008a6749
renzmann <32076780+renzmann@users.noreply.github.com>
2022-08-30 22:28:04 -0400

    add two tangle files

---
+++ content/posts/006_emacs_2_python/hello.py
@@ -0,0 +1,4 @@
+# [[file:index.org::*As a script with =M-x compile=][As a script with =M-x compile=:1]]
+# hello.py
+print("Hello, world!")
+# As a script with =M-x compile=:1 ends here
---
+++ content/posts/006_emacs_2_python/hello_error.py
@@ -0,0 +1,6 @@
+# [[file:index.org::*As a script with =M-x compile=][As a script with =M-x compile=:2]]
+# hello_error.py
+print("Not an error yet!")
+fdafdsafdsafdsa
+print("Shouldn't make it here...")
+# As a script with =M-x compile=:2 ends here
