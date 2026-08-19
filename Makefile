ffea2b7f744b778d63e9a9f081a39b47214e1f70
Robb Enzmann <robbenzmann@gmail.com>
2026-08-19 10:30:35 -0400

    Delete everything

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: clean
+clean:
+	rm -rf build/
+
+.PHONY: all
+all:
--- content/posts/006_emacs_2_python/hello_error.py
+++
@@ -1,6 +0,0 @@
-# [[file:index.org::*As a script with =M-x compile=][As a script with =M-x compile=:1]]
-# hello_error.py
-print("Not an error yet!")
-fdafdsafdsafdsa
-print("Shouldn't make it here...")
-# As a script with =M-x compile=:1 ends here
